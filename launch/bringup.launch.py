c30e916e504a6eb0eaacc32efb35a50c1221a1a5
Adarsh Karan Kesavadas Prasanth <adarshkaran01@gmail.com>
2025-03-20 20:08:50 +0530

    Completed migration to modern gazebo, reafactore launch files, modularized and restructured urdf

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -1,54 +1,173 @@
 # Neobotix GmbH
+# Author: Pradheep Padmanabhan
+# Contributor: Adarsh Karan K P
 
 import launch
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration
-from launch_ros.actions import Node
+import xacro
 import os
 from pathlib import Path
 
-def generate_launch_description():
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+  DeclareLaunchArgument,
+  IncludeLaunchDescription,
+  OpaqueFunction
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+
+from launch.launch_context import LaunchContext
+from launch.conditions import IfCondition
+
+def execution_stage(context: LaunchContext,
+                    robot_namespace,
+                    imu_enable,
+                    uss_enable,
+                    scanner_type):
+    
+    imu_enabl = str(imu_enable.perform(context))
+    uss_enabl = str(uss_enable.perform(context))
+    scanner_typ = str(scanner_type.perform(context))
     neo_mp_500 = get_package_share_directory('neo_mp_500-2')
 
-    urdf = os.path.join(get_package_share_directory('neo_mp_500-2'), 'robot_model/mp_500', 'mp_500.urdf')
+    rp_ns = ""
+    if (robot_namespace.perform(context) != "/"):
+        rp_ns = robot_namespace.perform(context) + "/"
 
-    with open(urdf, 'r') as infp:  
-        robot_desc = infp.read() # Dummy to use parameter instead of using argument=[urdf] in Node. Reference page: https://github.com/ros2/demos/pull/426/commits/a35a25732159e4c8b5655755ce31ec4c3e6e7975
+    launches = []
 
-    rsp_params = {'robot_description': robot_desc}
-
+    # Setting up the URDF
+    urdf = os.path.join(neo_mp_500,
+        'robot_model/mp_500',
+        'mp_500.urdf.xacro')
+    
+    # Start robot state publisher
     start_robot_state_publisher_cmd = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='screen',
-        parameters=[rsp_params])
+        namespace=robot_namespace,
+        parameters=[{
+            'robot_description': Command([
+                "xacro", " ", urdf,
+                " ", 'use_imu:=', imu_enabl,
+                " ", 'use_uss:=', uss_enabl,
+                " ", 'scanner_type:=', scanner_typ,
+            ]),
+            'frame_prefix': rp_ns
+        }],
+        arguments=[urdf]
+        )
 
+    launches.append(start_robot_state_publisher_cmd)
+
+    # 4. Laser
+    laser = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(neo_mp_500, 'configs/lidar/sick/s300', f'{scanner_typ}.launch.py')
+            ),
+            launch_arguments={
+                'namespace': robot_namespace
+            }.items()
+        )
+    launches.append(laser)
+
+    # 5. IMU
+    imu = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(neo_mp_500,
+                    'configs/phidget_imu',
+                    'imu_launch.py')
+            ),
+            launch_arguments={
+                'namespace': robot_namespace
+            }.items(),
+            condition=IfCondition(imu_enable)
+        )
+    
+    launches.append(imu)
+
+    return launches
+
+def generate_launch_description():
+    neo_mp_500 = get_package_share_directory('neo_mp_500-2')
+
+    # Launch configurations
+    robot_namespace = LaunchConfiguration('robot_namespace')
+    imu_enable = LaunchConfiguration('imu_enable')
+    uss_enable = LaunchConfiguration('uss_enable')
+    scanner_type = LaunchConfiguration('scanner_type')
+
+
+    context_arguments = [robot_namespace, imu_enable, uss_enable, scanner_type]
+
+    # Declare the launch arguments
+    declare_namespace_cmd = DeclareLaunchArgument(
+            'robot_namespace', default_value='', 
+            description='Top-level namespace'
+        )
+    
+    declare_imu_cmd = DeclareLaunchArgument(
+            'imu_enable', default_value='False',
+            description='Enable IMU - Options: True/False'
+        )
+    
+    declare_uss_cmd = DeclareLaunchArgument(
+            'uss_enable', default_value='False',
+            description='Enable uss - Options: True/False'
+        )
+
+    declare_scanner_type_cmd = DeclareLaunchArgument(
+            'scanner_type', default_value='sick_s300',
+            choices=['', 'sick_s300', 'sick_microscan3'],
+            description='Type of laser scanner to use'
+        )
+    
+    #  Launch hardware nodes
+    # 1. Relayboard
     relayboard = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(neo_mp_500, 'configs/relayboard_v2', 'relayboard_v2.launch.py')
-            )
+            ),
+            launch_arguments={
+                'namespace': robot_namespace
+            }.items()
         )
 
+    # 2. Kinematics
     kinematics = IncludeLaunchDescription(
-             PythonLaunchDescriptionSource(
-                 os.path.join(neo_mp_500, 'configs/kinematics', 'kinematics.launch.py')
-             )
-         )
+            PythonLaunchDescriptionSource(
+                os.path.join(neo_mp_500, 'configs/kinematics', 'kinematics.launch.py')
+            ),
+            launch_arguments={
+                'namespace': robot_namespace
+            }.items()
+        )
+
+    # 3. Teleop
     teleop = IncludeLaunchDescription(
              PythonLaunchDescriptionSource(
                  os.path.join(neo_mp_500, 'configs/teleop', 'teleop.launch.py')
-             )
-         )
-
-    laser = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(neo_mp_500, 'configs/lidar/sick/s300', 'sick_s300.launch.py')
-            )
+            ),
+            launch_arguments={
+                'namespace': robot_namespace
+            }.items()
         )
+    
+    # Opaque function for configuring URDF, IMU, Realsense and the USBoard
+    opq_function = OpaqueFunction(function=execution_stage, args=context_arguments)
 
+    ld = LaunchDescription()
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_imu_cmd)
+    ld.add_action(declare_uss_cmd)
+    ld.add_action(declare_scanner_type_cmd)
+    ld.add_action(relayboard)
+    ld.add_action(kinematics)
+    ld.add_action(teleop)
+    ld.add_action(opq_function)
 
-    return LaunchDescription([relayboard, start_robot_state_publisher_cmd, laser, kinematics, teleop])
+    return ld
